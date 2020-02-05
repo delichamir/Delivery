e14dfbff983b020fa8ba4e1faaaa099f4fc95af7
Amir Delich <amix.1011000@gmail.com>
2020-02-05 20:59:28 +0300

    fixing file of migration

--- migration/1580912108354-dump.ts
+++ migration/1580912108354-dump.ts
@@ -18,21 +18,24 @@ export class dump1580912108354 implements MigrationInterface {
       "name" text),
       "email" varchar(255) DEFAULT NULL,
       "register_date" timestamp NULL DEFAULT NULL,
-      "token" text `);
+      "token" text ,
+      PRIMARY KEY ("id") `);
     // Create table basket
     await queryRunner.query(`CREATE TABLE "basket" (
       "id" int(11) NOT NULL AUTO_INCREMENT,
       "object_id" int(11) DEFAULT NULL,
       "status" text,
       "update_time" timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-      "create_date" timestamp NULL DEFAULT NULL, `);
+      "create_date" timestamp NULL DEFAULT NULL,
+      PRIMARY KEY ("id") `);
     // Create table orders
     await queryRunner.query(`CREATE TABLE "orders" (
       "id" int(11) NOT NULL AUTO_INCREMENT,
       "object_id" int(11) DEFAULT NULL,
       "status" text,
       "update_time" timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-      "order_date" timestamp NULL DEFAULT NULL, `); 
+      "order_date" timestamp NULL DEFAULT NULL,
+      PRIMARY KEY ("id") `); 
     // Create table menu 
     await queryRunner.query(`CREATE TABLE "menu" (
       "id" int(11) NOT NULL AUTO_INCREMENT,
@@ -40,13 +43,15 @@ export class dump1580912108354 implements MigrationInterface {
       "dish_name" text,
       "category" text,
       "price" decimal(10,2) DEFAULT '0.00',
-      "count" int(20) DEFAULT NULL `);
+      "count" int(20) DEFAULT NULL,
+      PRIMARY KEY ("id") `);
     // Create table restaurants
     await queryRunner.query(`CREATE TABLE "restaurants" (
       "id" int(11) NOT NULL AUTO_INCREMENT,
       "object_id" int(11) DEFAULT NULL,
       "name" text,
-      "address" text `);   
+      "address" text,
+      PRIMARY KEY ("id") `);  
 
   }
 
@@ -62,29 +67,3 @@ export class dump1580912108354 implements MigrationInterface {
 
 
 
-CREATE TABLE `clients` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `object_id` int(11) DEFAULT NULL,
-  `login` varchar(20) DEFAULT NULL,
-  `password` text,
-  `email` varchar(255) DEFAULT NULL,
-  `first_name` text,
-  `father_name` text,
-  `last_name` text,
-  `balance` decimal(10,2) DEFAULT '0.00',
-  `money_reach` decimal(10,2) DEFAULT '0.00',
-  `money_spent` decimal(10,2) DEFAULT '0.00',
-  `tmp_money_spent` decimal(10,2) DEFAULT '0.00',
-  `tmp_insurance` decimal(10,2) DEFAULT NULL,
-  `money_update_time` timestamp NULL DEFAULT NULL,
-  `time_spent` bigint(20) DEFAULT '0',
-  `gooddeeds` decimal(10,2) DEFAULT NULL,
-  `car_id` bigint(20) DEFAULT NULL,
-  `car_rent_start_time` timestamp NULL DEFAULT NULL,
-  `agree` tinyint(1) DEFAULT NULL,
-  `book_count` tinyint(4) DEFAULT '0',
-  `tariff_type` tinyint(2) DEFAULT NULL,
-  `last_visit_time` timestamp NULL DEFAULT NULL,
-  `want_to_close_rent` tinyint(1) DEFAULT NULL,
-  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
